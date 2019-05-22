cfd398fde83d558c5712894bfed0f978ce05f9b3
Vladilen <mininvlad@gmail.com>
2019-05-22 13:56:38 +0300

    initial

--- public/app.js
+++ public/app.js
@@ -45,7 +45,7 @@ if ($card) {
                 <td>${c.title}</td>
                 <td>${c.count}</td>
                 <td>
-                  <button class="btn btm-small js-remove" data-id="${c.id}">Удалить</button>
+                  <button class="btn btm-small js-remove" data-id="${c.id}" data-csrf="${csrf}">Удалить</button>
                 </td>
               </tr>
               `
